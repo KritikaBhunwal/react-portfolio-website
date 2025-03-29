ff4d446370e4a96201f38a9eee789a302140ea62
KritikaBhunwal <kritikabhunwal@gmail.com>
2025-03-29 02:10:14 -0700

    fixed routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home";
-// import UIUXIndex from "./pages/uiux/UIUX";
-// import JavaScriptGames from "./pages/uiux/JavaScriptGames";
-// import Hellow from "./pages/uiux/Hellow";
-// import SmokingGun from "./pages/uiux/SmokingGun";
-// import GraphicsIndex from "./pages/graphics/Graphics";
-// import LogoDesign from "./pages/graphics/LogoDesign";
-// import Fashion from "./pages/fashion/Fashion";
-// import About from "./pages/about/About";
+import UIUXIndex from "./pages/uiux/UIUX";
+import JavaScriptGames from "./pages/uiux/JavaScriptGames";
+import Hellow from "./pages/uiux/Hellow";
+import SmokingGun from "./pages/uiux/SmokingGun";
+import GraphicsIndex from "./pages/graphics/Graphics";
+import LogoDesign from "./pages/graphics/LogoDesign";
+import Fashion from "./pages/fashion/Fashion";
+import About from "./pages/about/About";
 
 // Shared layout
 import Navbar from "./components/Navbar";
@@ -24,16 +24,16 @@ const AppRoutes = () => {
       <Routes>
         {/* Top-level pages */}
         <Route path="/" element={<Home />} />
-        {/* <Route path="/uiux" element={<UIUXIndex />} />
+        <Route path="/uiux" element={<UIUXIndex />} />
         <Route path="/graphics" element={<GraphicsIndex />} />
         <Route path="/fashion" element={<Fashion />} />
-        <Route path="/about" element={<About />} /> */}
+        <Route path="/about" element={<About />} />
 
         {/* Standalone subpages */}
-        {/* <Route path="/uiux/javascript-games" element={<JavaScriptGames />} />
+        <Route path="/uiux/javascript-games" element={<JavaScriptGames />} />
         <Route path="/uiux/hellow" element={<Hellow />} />
         <Route path="/uiux/smoking-gun" element={<SmokingGun />} />
-        <Route path="/graphics/logo-design" element={<LogoDesign />} /> */}
+        <Route path="/graphics/logo-design" element={<LogoDesign />} />
       </Routes>
       <Footer />
     </Router>
